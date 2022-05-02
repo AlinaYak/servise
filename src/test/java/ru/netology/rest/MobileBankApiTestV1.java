70e7647b9f572c695f388633889b544e20fd61eb
Alina1103master <honor10alina@gmail.ru>
2022-05-02 22:06:15 +0300

    second commit

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -3,6 +3,8 @@ package ru.netology.rest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
 
 class MobileBankApiTestV1 {
     @Test
@@ -17,5 +19,6 @@ class MobileBankApiTestV1 {
       // Проверки
       .then()
           .statusCode(200);
+      .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
     }
 }
